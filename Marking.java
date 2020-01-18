c9e574fe83fa2dc67143411e93e4baee1ba5bfa6
Esoha <ies@nur.kz>
2020-01-18 11:07:08 +0500

    Update Marking.java

--- Marking.java
+++ Marking.java
@@ -1,4 +1,13 @@
-  public class Marking
+ /****    
+ * Name: Zhuldyz Ilyassova
+ * Student Number: XXXXXXX
+ * Assignment #1: 1 D and 2 D Arrays
+ * Due Date:  January 21, 2020
+ * Program Description:  Exam test with score output for all students who taken exam
+ ****/ 
+
+
+public class Marking
   {
      //data
        private String [] names;
